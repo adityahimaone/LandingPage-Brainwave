620f3f25477da4176cf064df198a73c1f35c7e56
adityahimaone <adityahimaone@gmail.com>
2024-04-02 12:18:34 +0700

    feat: add section services

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -3,12 +3,14 @@ import { cn } from '../lib/utils';
 interface IProps {
   className?: string;
   title: string;
+  text?: string;
 }
 
-const Heading = ({ className, title }: IProps) => {
+const Heading = ({ className, title, text }: IProps) => {
   return (
     <div className={cn('mx-auto mb-12 max-w-[50rem] lg:mb-20', className)}>
       {title && <h2 className="h2">{title}</h2>}
+      {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
     </div>
   );
 };
